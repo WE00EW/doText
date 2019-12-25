102c524d6ad0a2a6f53cc4c61eb3d49d339b085e
周兴邦 <v_xbzzhou@tencent.com>
2019-12-25 17:56:07 +0800

    gorm

--- go.mod
+++ go.mod
@@ -4,6 +4,7 @@ go 1.13
 
 require (
 	github.com/gin-gonic/gin v1.5.0
+	github.com/jinzhu/gorm v1.9.11 // indirect
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.8.1
 	github.com/sirupsen/logrus v1.4.2 // indirect
---
+++ system/gorm/gorm.go
@@ -0,0 +1,34 @@
+package gorm
+
+import "github.com/jinzhu/gorm"
+
+type Product struct {
+	gorm.Model
+	Code string
+	Price uint
+}
+
+func initGORM() {
+	db, err := gorm.Open("sqlite3", "test.db")
+	if err != nil {
+		panic("连接数据库失败")
+	}
+	defer db.Close()
+
+	// 自动迁移模式
+	db.AutoMigrate(&Product{})
+
+	// 创建
+	db.Create(&Product{Code: "L1212", Price: 1000})
+
+	// 读取
+	var product Product
+	db.First(&product, 1) // 查询id为1的product
+	db.First(&product, "code = ?", "L1212") // 查询code为l1212的product
+
+	// 更新 - 更新product的price为2000
+	db.Model(&product).Update("Price", 2000)
+
+	// 删除 - 删除product
+	db.Delete(&product)
+}
